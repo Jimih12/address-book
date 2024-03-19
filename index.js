fdfcd880bab8229182662ec06fece778dffdec02
Jimih12 <setiaman.jimih@gmail.com>
2024-03-19 21:59:30 +0900

    address book

---
+++ index.js
@@ -0,0 +1,17 @@
+const contacts = [
+    {
+        id: 1,
+        fullName: "Jimih Setiawan",
+        age: 35,
+        phone: "+62-81234567890",
+    },
+    {
+        id: 2,
+        fullName: "Luqman",
+        age: 5,
+        phone: "+62-8123467890",
+    },
+];
+
+console.log(contacts);
+console.log(contacts[1]);
